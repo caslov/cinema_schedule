ab26a601ce4eb9cf778ca3a545956f3b4505ac3d
KasymovS <kasymov.sanzhar.t@gmail.com>
2022-10-19 21:44:56 +0600

    Good ex 10

--- mysite/logbook/urls.py
+++ mysite/logbook/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 from .views import *
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="Menu.html"), name="Menu"),
     path('movie/create/', MovieCreate.as_view()),
     path('movie/<int:pk>/', MovieDetail.as_view()),
     path('movie/list/', MovieList.as_view()),
